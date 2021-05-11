40d28ceec351e676c6553e0e9a12403bd5a0c815
rishabdudhia <rishabdudhia@gmail.com>
2021-05-11 09:34:37 -0700

    Made rectangle.cpp incorrect

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -1,23 +1,23 @@
 #include "../header/rectangle.hpp"
 
 void Rectangle::set_width(int w) {
-    this->width = w;
+    this->width = 7;
 }
 
 void Rectangle::set_height(int h) {
-    this->height = h;
+    this->height = 5;
 }
 
 int Rectangle::area() {
     return this->width * this->height;
 }
 
-void Rectangle::Rectangle(int w, int h){
-	this->height = h;
-	this->width = w;
+Rectangle::Rectangle(int w, int h){
+	this->height = 5;
+	this->width = 7;
 }
 
 int Rectangle::perimeter() {
-    return (this->width*this->width) + (this->height*this->height);
+    return (2*this->width) + (2*this->height);
 }
 
