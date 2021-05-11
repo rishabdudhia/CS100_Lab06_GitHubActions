77e00df45e4ad44d88e7a3b72755c37ec1d8c2a8
rishabdudhia <81605069+rishabdudhia@users.noreply.github.com>
2021-05-11 13:56:35 -0700

    Update test_constructors.hpp

--- tests/test_constructors.hpp
+++ tests/test_constructors.hpp
@@ -1,41 +1,31 @@
 #include "../header/rectangle.hpp"
 #include "gtest/gtest.h"
 
-TEST (TestConstructor, Normal){
+TEST (TestConstructor, NoInitialization){
 	Rectangle* rect1 = new Rectangle();
-	Rectangle* rect2 = new Rectangle(5,3);
-
-	rect1->set_width(5);
+	rect1->set_width(4);
 	rect1->set_height(3);
-
-	EXPECT_EQ(rect1->perimeter(), rect2->perimeter());
+	int expected = 14;
+ 
+	EXPECT_EQ(rect1->perimeter(), expected);
 	
 	delete rect1;
-	delete rect2;
 }
 
 TEST (TestConstructor, NegativeValues) {
-	Rectangle* rect1 = new Rectangle();
 	Rectangle* rect2 = new Rectangle(-2,-6);
+	int expected = 12;
 
-	rect1->set_width(-2);
-        rect1->set_height(-6);
+        EXPECT_EQ(expected, rect2->area());
 
-        EXPECT_EQ(rect1->perimeter(), rect2->perimeter());
-
-        delete rect1;
         delete rect2;
 }
 
 TEST (TestConstructor, Zero){
-	Rectangle* rect1 = new Rectangle();
-        Rectangle* rect2 = new Rectangle(0,0);
+	Rectangle* rect2 = new Rectangle(0,0);
+	int expected = 0;
 
-        rect1->set_width(0);
-        rect1->set_height(0);
+        EXPECT_EQ(expected, rect2->area());
 
-        EXPECT_EQ(rect1->area(), rect2->area());
-
-        delete rect1;
         delete rect2;
 }
