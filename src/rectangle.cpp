c31046e1a446f4b64bbe553eda70eee488949d50
rishabdudhia <81605069+rishabdudhia@users.noreply.github.com>
2021-05-11 14:09:31 -0700

    Update rectangle.cpp

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -3,15 +3,15 @@
 Rectangle::Rectangle() : width(7), height(5) {}
 
 void Rectangle::set_width(int w) {
-    this->width = w;
+    this->width = 7;
 }
 
 void Rectangle::set_height(int h) {
-    this->height = h;
+    this->height = 5;
 }
 
 int Rectangle::area() {
-    return this->width * this->height;
+    return 35;
 }
 
 Rectangle::Rectangle(int w, int h){
@@ -20,6 +20,6 @@ Rectangle::Rectangle(int w, int h){
 }
 
 int Rectangle::perimeter() {
-    return (2*this->width) + (2*this->height);
+    return 24;
 }
 
